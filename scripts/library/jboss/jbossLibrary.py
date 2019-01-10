93c1ab011a0f499fecb7d6402f4388ccf12a1986
x <x@x.com>
2019-01-10 10:45:46 +0000

    Pega may not always use the same setup for all Datasources, though it is likely. Cater for Pega's main datasources for performance options.

--- scripts/library/jboss/jbossLibrary.py
+++ scripts/library/jboss/jbossLibrary.py
@@ -1516,10 +1516,6 @@ def perfEnrichXAOracleDatasource(servername, username, password):
                 "password=" + dsPassword + ',' +
                 "spy=false, \
                 wrap-xa-resource=true, \
-<<<<<<< HEAD
-                max-pool-size=20, \
-=======
->>>>>>> branch 'master' of https://github.com/musesoftltd/JBoss-wildfly
                 set-tx-query-timeout=false, \
                 enabled=false, \
                 same-rm-override=false, \
