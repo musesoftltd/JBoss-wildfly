d52e619ac33415da049d526a1db4b0253e041847
x <x@x>
2019-06-19 21:29:41 +0100

    pega auditing crept in to JBoss server base auditing. Now separated.

--- scripts/library/auditing/auditServerBase.py
+++ scripts/library/auditing/auditServerBase.py
@@ -33,17 +33,7 @@ def auditServersBaseAudit(environment, servername, propertiesDict, bApplyRequire
     oAuditObjectMolecule2.auditObjectAtoms.append(auditObjectAtom(servername, runtimeProperties["username"], runtimeProperties["password"], "SSL Protocols", "/subsystem=web/connector=https/configuration=ssl/", "protocol", runtimeProperties["sslProtocols"], bApplyRequiredChanges))
     oAuditObjectMolecule2.auditObjectAtoms.append(auditObjectAtom(servername, runtimeProperties["username"], runtimeProperties["password"], "Cipher Suite", "/subsystem=web/connector=https/configuration=ssl/", "cipher-suite", runtimeProperties["cipherSuite"], bApplyRequiredChanges))
 
-#     allDatasourcesResponseResultList = getAllDataSources(servername, runtimeProperties["username"], runtimeProperties["password"])
-    allDatasourcesResponseResultList = [
-        'StagingInbound',
-        'AdminPegaRULES',
-        'PegaRULES',
-        'adm7DataSource',
-        'ihDataSource',
-        'nbamDataSource',
-        'padDataSource',
-        'vbdDataSource',
-        ]
+    allDatasourcesResponseResultList = getAllDataSources(servername, runtimeProperties["username"], runtimeProperties["password"])
 
     if (allDatasourcesResponseResultList) :
         oAuditObjectMolecule3 = auditObjectMolecule("Datasource (Non XA) Connection Perf Options", servername, True)
